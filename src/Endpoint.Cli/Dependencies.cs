ea72bdc4838b065f0837fa41a99aa2eb4882d391
Quinntyne Brown <quinntynebrown@gmail.com>
2022-02-06 00:12:59 -0500

    fix: plugin

--- src/Endpoint.Cli/Dependencies.cs
+++ src/Endpoint.Cli/Dependencies.cs
@@ -1,13 +1,12 @@
 ﻿using Endpoint.Application;
 using Endpoint.Application.Core.Extenstions;
-using Endpoint.Application.Plugin.ContentManagement;
-using Endpoint.Application.Plugin.Identity;
 using Endpoint.SharedKernal;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Linq;
-
+using System.IO;
+using System.Reflection;
+using System.Runtime.Loader;
 
 namespace Endpoint.Cli
 {
@@ -19,20 +18,60 @@ namespace Endpoint.Cli
             services.AddMediatR(typeof(Dependencies).Assembly);
             services.AddSingleton<IDomainEvents, DomainEvents>();
             services.AddSharedServices();
-            services.AddCoreServices();
+            services.AddCoreServices();      
             
-            if(plugins.Contains("Identity"))
+            foreach(var plugin in plugins)
             {
-                services.AddMediatR(typeof(IdentityPluginEventHandler));
-            }
-
-            if (plugins.Contains("ContentManagement"))
-            {
-                services.AddMediatR(typeof(ContentManagementPluginEventHandler));
+                services.AddMediatR(LoadPlugin(@$"Plugins\Endpoint.Application.Plugin.{plugin}\bin\Debug\net5.0\Endpoint.Application.Plugin.{plugin}.dll"));
             }
         }
 
+        static Assembly LoadPlugin(string relativePath)
+        {
+            string root = Path.GetFullPath(Path.Combine(
+                Path.GetDirectoryName(
+                    Path.GetDirectoryName(
+                        Path.GetDirectoryName(
+                            Path.GetDirectoryName(
+                                Path.GetDirectoryName(typeof(Program).Assembly.Location)))))));
+
+            string pluginLocation = Path.GetFullPath(Path.Combine(root, relativePath.Replace('\\', Path.DirectorySeparatorChar)));
+            Console.WriteLine($"Loading commands from: {pluginLocation}");
+            PluginLoadContext loadContext = new PluginLoadContext(pluginLocation);
+            return loadContext.LoadFromAssemblyName(new AssemblyName(Path.GetFileNameWithoutExtension(pluginLocation)));
+        }
     }
 
 
+    class PluginLoadContext : AssemblyLoadContext
+    {
+        private AssemblyDependencyResolver _resolver;
+
+        public PluginLoadContext(string pluginPath)
+        {
+            _resolver = new AssemblyDependencyResolver(pluginPath);
+        }
+
+        protected override Assembly Load(AssemblyName assemblyName)
+        {
+            string assemblyPath = _resolver.ResolveAssemblyToPath(assemblyName);
+            if (assemblyPath != null)
+            {
+                return LoadFromAssemblyPath(assemblyPath);
+            }
+
+            return null;
+        }
+
+        protected override IntPtr LoadUnmanagedDll(string unmanagedDllName)
+        {
+            string libraryPath = _resolver.ResolveUnmanagedDllToPath(unmanagedDllName);
+            if (libraryPath != null)
+            {
+                return LoadUnmanagedDllFromPath(libraryPath);
+            }
+
+            return IntPtr.Zero;
+        }
+    }
 }
