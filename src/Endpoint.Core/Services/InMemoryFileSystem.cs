0e2d2788f439ba4774b85717704b9e66553045e2
Quinntyne Brown <quinntynebrown@gmail.com>
2022-02-13 09:59:20 -0500

    add inmemory file system

---
+++ src/Endpoint.Core/Services/InMemoryFileSystem.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Endpoint.Core.Services
+{
+    public class InMemoryFileSystem: IFileSystem
+    {
+        private readonly IDictionary<string, FileSystemEntry> files = new Dictionary<string, FileSystemEntry>();
+
+        public bool Exists(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists(string[] paths)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Stream OpenRead(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ParentFolder(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ReadAllText(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void WriteAllLines(string path, string[] contents)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class FileSystemEntry
+    {
+        public FileAttributes Attributes { get; set; }
+        public DateTimeOffset CreationTimeUtc { get; set; }
+        public string Directory { get; set; }
+        public string FileName { get; set; }
+        public bool IsDirectory { get; set; }
+        public bool IsHidden { get; set; }
+        public DateTimeOffset LastAccessTimeUtc { get; set; }
+        public DateTimeOffset LastWriteTimeUtc { get; set; }
+        public long Length { get; set; }
+        public string OriginalRootDirectory { get; set; }
+        public string RootDirectory { get; set; }
+    }
+}
