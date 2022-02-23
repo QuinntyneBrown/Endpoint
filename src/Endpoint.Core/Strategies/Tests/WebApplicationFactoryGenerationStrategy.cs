7831b093fa1c88d4b85eac7f1f0ddca52861225a
Quinntyne Brown <quinntynebrown@gmail.com>
2022-02-23 18:30:35 -0500

    feat: test generation

---
+++ src/Endpoint.Core/Strategies/Tests/WebApplicationFactoryGenerationStrategy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Endpoint.Core.Strategies.Tests
+{
+    public interface IWebApplicationFactoryGenerationStrategy
+    {
+
+    }
+
+    public class WebApplicationFactoryGenerationStrategy: IWebApplicationFactoryGenerationStrategy
+    {
+    }
+}
