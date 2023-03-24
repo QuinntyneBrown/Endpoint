e2346308ebb75a0c17fea5f9b99d6ec044c02267
Quinntyne Brown <quinntynebrown@gmail.com>
2023-03-23 21:15:24 -0400

    update

--- src/Endpoint.Cli/Commands/ReferenceAdd.cs
+++ src/Endpoint.Cli/Commands/ReferenceAdd.cs
@@ -2,20 +2,20 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using CommandLine;
-using Endpoint.Core.Services;
 using MediatR;
-using Microsoft.Extensions.Logging;
 using System;
-using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
 
 namespace Endpoint.Cli.Commands;
 
-[Verb("ref-add")]
+
+[Verb("reference-add")]
 public class ReferenceAddRequest : IRequest {
-    [Option('s',"service-directory")]
-    public string ServiceDirectory { get; set; }
+    [Option('n',"name")]
+    public string Name { get; set; }
 
 
     [Option('d', Required = false)]
@@ -25,25 +25,14 @@ public class ReferenceAddRequest : IRequest {
 public class ReferenceAddRequestHandler : IRequestHandler<ReferenceAddRequest>
 {
     private readonly ILogger<ReferenceAddRequestHandler> _logger;
-    private readonly ICommandService _commandService;
-    private readonly IFileProvider _fileProvider;
-    public ReferenceAddRequestHandler(
-        ILogger<ReferenceAddRequestHandler> logger,
-        ICommandService commandService,
-        IFileProvider fileProvider)
+
+    public ReferenceAddRequestHandler(ILogger<ReferenceAddRequestHandler> logger)
     {
         _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-        _commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
-        _fileProvider = fileProvider ?? throw new ArgumentNullException(nameof(fileProvider));
     }
 
     public async Task Handle(ReferenceAddRequest request, CancellationToken cancellationToken)
     {
         _logger.LogInformation("Handled: {0}", nameof(ReferenceAddRequestHandler));
-
-        foreach(var path in Directory.GetFiles(request.ServiceDirectory,"*.csproj", SearchOption.AllDirectories))
-        {
-            _commandService.Start($"dotnet add {request.Directory} reference {path}", request.Directory);
-        }
     }
 }
---
+++ src/Endpoint.Cli/Commands/SpecCreate.cs
@@ -0,0 +1,59 @@
+// Copyright (c) Quinntyne Brown. All Rights Reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using CommandLine;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Endpoint.Core.Services;
+using Endpoint.Core.Models.Artifacts.Files.Factories;
+using Endpoint.Core.Abstractions;
+
+namespace Endpoint.Cli.Commands;
+
+
+[Verb("spec-create")]
+public class SpecCreateRequest : IRequest {
+    [Option('n',"name")]
+    public string Name { get; set; }
+
+
+    [Option('d', Required = false)]
+    public string Directory { get; set; } = System.Environment.CurrentDirectory;
+}
+
+public class SpecCreateRequestHandler : IRequestHandler<SpecCreateRequest>
+{
+    private readonly ILogger<SpecCreateRequestHandler> _logger;
+    private readonly INamingConventionConverter _namingCoventionConverter;
+    private readonly IFileModelFactory _fileModelFactory;
+    private readonly IArtifactGenerationStrategyFactory _artifactGenerationStrategyFactory;
+    public SpecCreateRequestHandler(
+        ILogger<SpecCreateRequestHandler> logger,
+        INamingConventionConverter namingCoventionConverter,
+        IFileModelFactory fileModelFactory,
+        IArtifactGenerationStrategyFactory artifactGenerationStrategyFactory)
+    {
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        _namingCoventionConverter = namingCoventionConverter ?? throw new ArgumentNullException(nameof(namingCoventionConverter));
+        _fileModelFactory = fileModelFactory ?? throw new ArgumentNullException(nameof(fileModelFactory));
+        _artifactGenerationStrategyFactory = artifactGenerationStrategyFactory ?? throw new ArgumentNullException(nameof(artifactGenerationStrategyFactory));
+    }
+
+    public async Task Handle(SpecCreateRequest request, CancellationToken cancellationToken)
+    {
+        _logger.LogInformation("Handled: {0}", nameof(SpecCreateRequestHandler));
+
+        var fileName = $"{_namingCoventionConverter.Convert(NamingConvention.SnakeCase, request.Name.Remove("Service"))}.service";
+
+        var model = _fileModelFactory.CreateTemplate("Angular.Services.Default.Spec", $"{fileName}.spec", request.Directory, "ts", tokens: new TokensBuilder()
+            .With("name", request.Name)
+            .With("fileName", fileName)
+            .Build());
+
+        _artifactGenerationStrategyFactory.CreateFor(model);
+
+    }
+}
