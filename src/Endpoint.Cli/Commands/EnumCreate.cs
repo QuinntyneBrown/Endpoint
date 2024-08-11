a05e5600c2be857f0510f83b532863dfe8c6cde0
Quinntyne Brown <quinntynebrown@gmail.com>
2024-08-11 02:35:52 -0400

    Develop (#31)
    * add async events
    
    * add code formatting
    
    * update

--- src/Endpoint.Cli/Commands/EnumCreate.cs
+++ src/Endpoint.Cli/Commands/EnumCreate.cs
@@ -1,20 +1,20 @@
 // Copyright (c) Quinntyne Brown. All Rights Reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using CommandLine;
 using Endpoint.Core.Artifacts;
 using Endpoint.Core.Artifacts.Files;
+using Endpoint.Core.Syntax;
 using Endpoint.Core.Syntax.Classes;
 using Endpoint.Core.Syntax.Constructors;
-using Endpoint.Core.Syntax.Fields;
-using Endpoint.Core.Syntax.Params;
-using Endpoint.Core.Syntax.Types;
 using MediatR;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
+using static Endpoint.Core.Constants.FileExtensions;
 
 namespace Endpoint.Cli.Commands;
 
@@ -24,8 +24,11 @@ public class EnumCreateRequest : IRequest
     [Option('n',"name")]
     public string Name { get; set; }
 
+    [Option('t', "type")]
+    public string Type { get; set; } = "int";
+
     [Option('e', "enums")]
-    public string Enums { get; set; }
+    public string Enums { get; set; } = string.Empty;
 
     [Option('d', Required = false)]
     public string Directory { get; set; } = System.Environment.CurrentDirectory;
@@ -35,6 +38,7 @@ public class EnumCreateRequestHandler : IRequestHandler<EnumCreateRequest>
 {
     private readonly ILogger<EnumCreateRequestHandler> _logger;
     private readonly IArtifactGenerator _generator;
+    private static readonly string[] supportedTypes = [ "int", "string" ];
 
     public EnumCreateRequestHandler(ILogger<EnumCreateRequestHandler> logger, IArtifactGenerator generator)
     {
@@ -49,90 +53,53 @@ public class EnumCreateRequestHandler : IRequestHandler<EnumCreateRequest>
     {
         _logger.LogInformation("Handled: {0}", nameof(EnumCreateRequestHandler));
 
-        var model = new ClassModel(request.Name);
-
-        model.Usings.Add(new ("System"));
-
-        int numberOfEnums = 0;
-        foreach (var @enum in request.Enums.Split(','))
+        if (!supportedTypes.Contains(request.Type))
         {
-            var parts = @enum.Split(':');
-
-            if (parts.Length != 2)
-            {
-                model.Fields.Add(new FieldModel()
-                {
-                    Name = parts[0],
-                    Static = true,
-                    AccessModifier = Core.Syntax.AccessModifier.Public,
-                    DefaultValue = $"new (1 << {numberOfEnums})",
-                    Type = new TypeModel(request.Name),
-                });
-            }
-
-            numberOfEnums++;
+            throw new NotSupportedException();
         }
 
-        var constructor = new ConstructorModel(model, model.Name)
+        var model = ClassModel.CreateUserDefinedEnum(request.Name, request.Type, request.Enums.Split(',').Select(x =>
         {
-            Params = new List<ParamModel>()
-            {
-                new ParamModel()
-                {
-                    Name = "value",
-                    Type = new TypeModel("int"),
-                },
-            },
-        };
+            var parts = x.Split(":");
 
-        model.Fields.Add(new FieldModel() {
-            Name = "_value",
-            AccessModifier = Core.Syntax.AccessModifier.Private,
-            ReadOnly = true,
-            Type = new TypeModel("int"),
-        });
+            var keyValuePair = new KeyValuePair<string, string>(parts[0], parts.Length > 1 ? parts[1] : string.Empty);
 
-        model.Methods.Add(new ()
-        {
-            ImplicitOperator = true,
-            Name = "int",
-            Static = true,
-            Params = new List<ParamModel>()
-            {
-                new ParamModel()
-                {
-                    Name = request.Name.ToCamelCase(),
-                    Type = new TypeModel(request.Name),
-                },
-            },
-            Body = new ($"return {request.Name.ToCamelCase()}._value;"),
-        });
+            return keyValuePair;
+        }));
 
-        model.Methods.Add(new ()
-        {
-            ExplicitOperator = true,
-            Name = request.Name,
-            Static = true,
-            Params = new List<ParamModel>()
-            {
-                new ParamModel()
-                {
-                    Name = "value",
-                    Type = new TypeModel("int"),
-                },
-            },
-            Body = new ($"return new {request.Name}(value);"),
-        });
-
-        model.Constructors.Add(constructor);
-
-        var classFile = new CodeFileModel<ClassModel>(
+        CodeFileModel<ClassModel> classFile = new (
             model,
             model.Usings,
             model.Name,
             request.Directory,
-            ".cs");
+            CSharpFile);
 
         await _generator.GenerateAsync(classFile);
     }
+}
+
+public class HotlineBling
+{
+    public static readonly HotlineBling Drake = new(6);
+
+    private readonly int _value;
+
+    public HotlineBling(int value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        _value = value;
+    }
+
+    public static implicit operator int(HotlineBling hotlineBling)
+    {
+        return hotlineBling._value;
+
+    }
+
+    public static explicit operator HotlineBling(int value)
+    {
+        return new HotlineBling(value);
+
+    }
+
 }
