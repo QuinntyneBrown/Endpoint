1df3a2cc6a6f55fe2b567c09df2709aef5a5d390
Quinntyne Brown <quinntynebrown@gmail.com>
2024-08-11 02:34:44 -0400

    update

--- src/Endpoint.Core/Syntax/Classes/ClassModel.cs
+++ src/Endpoint.Core/Syntax/Classes/ClassModel.cs
@@ -12,6 +12,87 @@ namespace Endpoint.Core.Syntax.Classes;
 
 public class ClassModel : InterfaceModel
 {
+    public static ClassModel CreateUserDefinedEnum(string name, string type, IEnumerable<KeyValuePair<string,string>> keyValuePairs)
+    {
+        var model = new ClassModel(name);
+
+        model.Usings.Add(new ("System"));
+
+        int numberOfEnums = 0;
+
+        foreach (var keyValuePair in keyValuePairs)
+        {
+            model.Fields.Add(new ()
+            {
+                Name = keyValuePair.Key,
+                Static = true,
+                AccessModifier = AccessModifier.Public,
+                Type = new(name),
+                DefaultValue = string.IsNullOrEmpty(keyValuePair.Value)
+                ? type == "int" ? $"new (1 << {numberOfEnums})" : $"new (nameof({keyValuePair.Key}))"
+                : type == "int" ? $"new ({keyValuePair.Value})" : $"new (\"{keyValuePair.Value}\")",
+            });
+
+            numberOfEnums++;
+        }
+
+        ConstructorModel constructor = new(model, model.Name)
+        {
+            Params =
+            [
+                new()
+                {
+                    Name = "value",
+                    Type = new(type),
+                },
+            ],
+        };
+
+        model.Fields.Add(new()
+        {
+            Name = "_value",
+            AccessModifier = AccessModifier.Private,
+            ReadOnly = true,
+            Type = new(type),
+        });
+
+        model.Methods.Add(new()
+        {
+            ImplicitOperator = true,
+            Name = type,
+            Static = true,
+            Params =
+            [
+                new()
+                {
+                    Name = name.ToCamelCase(),
+                    Type = new(name),
+                },
+            ],
+            Body = new($"return {name.ToCamelCase()}._value;"),
+        });
+
+        model.Methods.Add(new()
+        {
+            ExplicitOperator = true,
+            Name = name,
+            Static = true,
+            Params =
+            [
+                new()
+                {
+                    Name = "value",
+                    Type = new(type),
+                },
+            ],
+            Body = new($"return new {name}(value);"),
+        });
+
+        model.Constructors.Add(constructor);
+
+        return model;
+    }
+
     public ClassModel()
     {
     }
