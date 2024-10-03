4f817f5638d6e7e6a86b01433f5eeac8d11738be
Quinntyne Brown <quinntynebrown@gmail.com>
2024-10-02 22:00:17 -0400

    resolve

--- src/Endpoint.Core/Internal/AsyncEventInvocator.cs
+++ src/Endpoint.Core/Internal/AsyncEventInvocator.cs
@@ -1,10 +1,14 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
 namespace Endpoint.Internal;
 
 public readonly struct AsyncEventInvocator<TEventArgs>
 {
     readonly Action<TEventArgs> _handler;
     readonly Func<TEventArgs, Task> _asyncHandler;
-
+    
     public AsyncEventInvocator(Action<TEventArgs> handler, Func<TEventArgs, Task> asyncHandler)
     {
         _handler = handler;
@@ -16,13 +20,13 @@ public readonly struct AsyncEventInvocator<TEventArgs>
         // Do not use ReferenceEquals! It will not work with delegates.
         return handler == _handler;
     }
-
+    
     public bool WrapsHandler(Func<TEventArgs, Task> handler)
     {
         // Do not use ReferenceEquals! It will not work with delegates.
         return handler == _asyncHandler;
     }
-
+    
     public Task InvokeAsync(TEventArgs eventArgs)
     {
         if (_handler != null)
