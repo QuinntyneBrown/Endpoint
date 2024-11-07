fed569fdd9f756abf28339d28b076a3649f189d6
Quinntyne Brown <quinntynebrown@gmail.com>
2024-11-06 19:53:06 -0500

    resolve

--- src/Endpoint.DotNet/Events/EndpointEventContainer.cs
+++ src/Endpoint.DotNet/Events/EndpointEventContainer.cs
@@ -1,15 +1,9 @@
 // Copyright (c) Quinntyne Brown. All Rights Reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using Microsoft.Extensions.Logging;
-using System;
-using System.Threading.Tasks;
-
 namespace Endpoint.Core.Events;
 
 public class EndpointEventContainer: IEndpointEventContainer
 {
 
-
 }
-
