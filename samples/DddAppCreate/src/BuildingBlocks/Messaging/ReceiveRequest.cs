89d6a442d5a61b3f8086ae61a318b64c451e59bd
Quinntyne Brown <quinntynebrown@gmail.com>
2024-11-06 19:56:44 -0500

    Develop (#45)
    * resolve
    
    * resolve
    
    * update

--- samples/DddAppCreate/src/BuildingBlocks/Messaging/ReceiveRequest.cs
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Quinntyne Brown. All Rights Reserved.
-// Licensed under the MIT License. See License.txt in the project root for license information.
-
-namespace Messaging;
-
-public class ReceiveRequest { }
--- samples/update.bat
+++
@@ -1,3 +0,0 @@
-dotnet tool uninstall -g Quinntyne.Endpoint.Cli
-dotnet pack ../src/Endpoint.Cli/Endpoint.Cli.csproj
-dotnet tool install --global --add-source ../src/Endpoint.Cli/nupkg Quinntyne.Endpoint.Cli --version 0.1.9
