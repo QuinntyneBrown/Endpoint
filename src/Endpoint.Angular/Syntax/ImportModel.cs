fd40664d4d56b5c8508484bdbdb8e9d5cc4f69d2
Quinntyne Brown <quinntynebrown@gmail.com>
2025-11-18 20:54:49 -0500

    Index create (#77)
    * update
    
    * update
    
    * update
    
    * update

--- src/Endpoint.Angular/Syntax/ImportModel.cs
+++ src/Endpoint.Angular/Syntax/ImportModel.cs
@@ -1,18 +1,28 @@
 // Copyright (c) Quinntyne Brown. All Rights Reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
-using Endpoint.Core.Syntax;
-
 namespace Endpoint.Angular.Syntax;
 
 public class ImportModel
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ImportModel"/> class.
+    /// </summary>
+    /// <remarks>This constructor sets the <see cref="Types"/> property to an empty list and the <see
+    /// cref="Module"/> property to an empty string.</remarks>
     public ImportModel()
     {
         Types = [];
         Module = string.Empty;
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ImportModel"/> class with the specified type and module.
+    /// </summary>
+    /// <remarks>The constructor initializes the <see cref="Types"/> collection with the provided
+    /// type.</remarks>
+    /// <param name="type">The type to be imported. This parameter cannot be null or empty.</param>
+    /// <param name="module">The module associated with the import. This parameter cannot be null or empty.</param>
     public ImportModel(string type, string module)
     {
         Module = module;
@@ -22,7 +32,13 @@ public class ImportModel
         ];
     }
 
+    /// <summary>
+    /// Gets or sets the collection of type models.
+    /// </summary>
     public List<TypeModel> Types { get; set; }
 
+    /// <summary>
+    /// Gets or sets the module associated with the import.
+    /// </summary>
     public string Module { get; set; }
 }
--- src/Endpoint.Angular/Syntax/TypeScriptTypeModel.cs
+++ src/Endpoint.Angular/Syntax/TypeScriptTypeModel.cs
@@ -3,15 +3,31 @@
 
 namespace Endpoint.Angular.Syntax;
 
+/// <summary>
+/// Represents a model for a TypeScript type, including its name and associated properties.
+/// </summary>
+/// <remarks>This class is used to model TypeScript types within a syntax tree, allowing for the representation of
+/// a type's name and its properties. It is part of a larger system for analyzing or generating TypeScript
+/// code.</remarks>
 public class TypeScriptTypeModel : SyntaxModel
 {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TypeScriptTypeModel"/> class with the specified name.
+    /// </summary>
+    /// <param name="name">The name of the TypeScript type model. Cannot be null or empty.</param>
     public TypeScriptTypeModel(string name)
     {
         Name = name;
         Properties = [];
     }
 
+    /// <summary>
+    /// Gets or sets the name associated with the current instance.
+    /// </summary>
     public string Name { get; set; }
 
+    /// <summary>
+    /// Gets or sets the collection of property models.
+    /// </summary>
     public List<PropertyModel> Properties { get; set; }
 }
