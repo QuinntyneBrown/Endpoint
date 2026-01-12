c0531d738fec4a1276fe40d0b18fa6a0c61f30cc
Quinntyne Brown <quinntynebrown@gmail.com>
2026-01-12 06:36:29 -0500

    update

--- playground/EventDrivenMicroservices/output/EventDrivenDemo/README.md
+++
@@ -1,1 +0,0 @@
-# EventDrivenDemo
--- playground/EventDrivenMicroservices/output/EventDrivenDemo/src/BuildingBlocks/Messaging/Messaging/IServiceBusMessageListener.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Quinntyne Brown. All Rights Reserved.
-// Licensed under the MIT License. See License.txt in the project root for license information.
-
-namespace Messaging;
-
-public interface IServiceBusMessageListener : IObservable<IServiceBusMessage>
-{
-    Task StartAsync(CancellationToken cancellationToken = default);
-}
